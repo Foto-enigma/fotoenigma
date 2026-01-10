a99ab6943e66c5f60e73d2233e78b88e98933290
deverajuanpablo299@gmail.com <deverajuanpablo299@gmail.com>
2026-01-10 21:08:16 +0100

    estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import PrivacyEN from "./components/PrivacyEN";
+
 export default function App() {
   return (
-    <div></div>
+    <div className="app">
+      <h1 className="app-title">Privacy Policy
+        <span class="cursor"></span>
+      </h1>
+      <PrivacyEN />
+    </div>
   )
 }
---
+++ src/components/PrivacyEN.jsx
@@ -0,0 +1,133 @@
+export default function PrivacyEN() {
+    return (
+        <div className="privacy-data">
+            <div className="privacy-data-container 1">
+                <h1>Data Controller</h1>
+                <p>The data controller responsible for the processing of personal data is:
+                Name: Yadier de Vera
+                Contact email: fotoenigma.soporte@gmail.com
+                </p>
+            </div>
+
+            <div className="privacy-data-container 2">
+                <h1>Target audience</h1>
+                <p>
+                    FotoEnigma is not intended for children.
+                    The game is designed for users aged 13 and over.
+                </p>
+            </div>
+
+            <div className="privacy-data-container 3">
+                <h1>Information we collect</h1>
+                <p>The application may automatically collect information to improve user experience and game
+                performance.
+                Collected data includes:
+                </p>
+                <ul>
+                    <li>Game progress</li>
+                    <li>Played levels and usage statistics</li>
+                    <li>Playtime</li>
+                    <li>Device information (model, operating system, language)</li>
+                    <li>Advertising identifiers</li>
+                    <li>Information related to in-app purchases</li>
+                </ul>
+                <p>This data does not personally identify users.</p>
+            </div>
+
+            <div className="privacy-data-container 4">
+                <h1>Third-party services</h1>
+                <p>
+                    Unity Analytics
+                    We use Unity Analytics to:
+                </p>
+                <ul>
+                    <li>Analyze player behavior</li>
+                    <li>Identify most-played levels</li>
+                    <li>Improve gameplay and content</li>
+                    <p>Unity may collect anonymous usage data in accordance with its privacy policy.</p>
+                </ul>
+            </div>
+
+            <div className="privacy-data-container 5">
+                <h1>Google AdMob</h1>
+                <p>
+                    The app uses Google AdMob to display advertisements.
+                    AdMob may collect:
+                </p>
+                <ul>
+                    <li>Advertising identifiers</li>
+                    <li>Device information</li>
+                    <li>Ad interaction data</li>
+                </ul>
+                <p>More information:
+                    https://policies.google.com/privacy</p>
+            </div>
+
+            <div className="privacy-data-container 6">
+                <h1>In-app purchases</h1>
+                <p>
+                    FotoEnigma offers in-app purchases through Unity In-App Purchasing.
+                </p>
+                <ul>
+                    <li>We do not collect or store payment information</li>
+                    <li>All transactions are processed by Google Play</li>
+                </ul>
+            </div>
+
+            <div className="privacy-data-container 7">
+                <h1>Sharing feature - Native Share</h1>
+                <p>
+                    The game includes a sharing feature using the device’s native sharing system.
+                </p>
+                <ul>
+                    <li>Activated only by user action</li>
+                    <li>No personal data is collected when using this feature</li>
+                </ul>
+            </div>
+
+            <div className="privacy-data-container 8">
+                <h1>AI-generated images</h1>
+                <p>
+                    Images used in FotoEnigma are generated using Artificial Intelligence tools.
+
+                    These images:
+                </p>
+                <ul>
+                    <li>Do not represent real people</li>
+                    <li>Are used solely for entertainment purposes</li>
+                    <li>Are part of the original game content</li>
+                </ul>
+            </div>
+
+            <div className="privacy-data-container 9">
+                <h1>Data deletion</h1>
+                <p>
+                    Users may request data deletion by:
+                </p>
+                <ul>
+                    <li>Using the data deletion button available in the game settings</li>
+                    <li>Contacting us via email:</li>
+                </ul>
+                <p> fotoenigma.soporte@gmail.com
+                    Requests will be handled within a reasonable timeframe.</p>
+            </div>
+
+            <div className="privacy-data-container 10">
+                <h1>Data security</h1>
+                <p>Reasonable technical and organizational measures are applied to protect collected data.</p>
+            </div>
+
+            <div className="privacy-data-container 11">
+                <h1>Changes to this policy</h1>
+                <p>This Privacy Policy may be updated in the future.
+                    Any changes will be published on this page.</p>
+            </div>
+
+            <div className="privacy-data-container">
+                <h1>Contact</h1>
+                <p>For questions regarding this Privacy Policy, contact:
+                fotoenigma.soporte@gmail.com</p>
+            </div>
+        </div>
+    )
+}
